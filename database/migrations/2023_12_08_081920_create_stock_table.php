6257cd62dd069301ab8f73c7ef0f09a8cc19afdd
ramadikaar <127398948+ramadikaar@users.noreply.github.com>
2023-12-08 21:10:43 +0700

    menambahkan database

---
+++ database/migrations/2023_12_08_081920_create_stock_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stock', function (Blueprint $table) {
+            $table->id('stock_id');
+            $table->integer('product_id');
+            $table->integer('quantity');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stock');
+    }
+};
---
+++ database/migrations/2023_12_08_081933_create_transaction_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaction', function (Blueprint $table) {
+            $table->id('transaction_id');
+            $table->integer('customer_id');
+            $table->integer('product_id');
+            $table->integer('promo_id');
+            $table->dateTime('transaction_date');
+            $table->integer('total_amount');
+            $table->string('payment_method');
+            $table->string('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaction');
+    }
+};
